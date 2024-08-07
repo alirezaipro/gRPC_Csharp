cb7c61e49c2c42a51edf65d3ad591a2523e74b68
ali rezaei <alirezai.pro@gmail.com>
2024-08-08 01:54:03 +0330

    feat: add grpc reflection to server and use grpcui

--- gRPC.Server/Program.cs
+++ gRPC.Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using gRPC_Client.protos;
 using Grpc.Core;
 using gRPC.Server.Implementation;
+using Grpc.Reflection;
+using Grpc.Reflection.V1Alpha;
 
 const int port = 50051;
 
@@ -19,13 +21,17 @@ var credentials = new SslServerCredentials(new List<KeyCertificatePair>() {
 
 try
 {
+
+    var reflectionServiceImpl = new ReflectionServiceImpl(ProductService.Descriptor);
+
     server = new Server()
     {
         Services = {
             ProductService.BindService(new ProductServiceImp()),
-            MathService.BindService(new MathServiceImp())
+            MathService.BindService(new MathServiceImp()),
+            ServerReflection.BindService(reflectionServiceImpl)
         },
-        Ports = { new ServerPort("localhost", port, credentials) }
+        Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
     };
 
     server.Start();
